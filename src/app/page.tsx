3121f5297f1b41d23d88a82695b2a8f01f0f5256
unknown <chougulearvind1@gmail.com>
2024-12-24 20:57:21 -0800

    navbar added to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import Image from "next/image";
+import Link from "next/link";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../logo.png'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      {/*  <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -95,7 +100,96 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
+         <nav className="navbar_navigation__gxZGc navbar navbar-expand-xxl navbar-light fixed-top">
+      <div className="container">
+        {/* Logo */}
+        <Link href="/" className="navbar-brand">
+          <Image
+            alt="insta care Logo"
+            fetchPriority="high"
+            width={150}
+            height={46}
+            decoding="async"
+            src={logo}
+            style={{ color: "transparent" }}
+          />
+        </Link>
+
+        {/* Toggler Button */}
+        <button
+          aria-controls="offcanvasNavbar-expand-xxl"
+          type="button"
+          aria-label="Toggle navigation"
+          className="navbar-toggler collapsed"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Offcanvas Content */}
+        <div
+          id="offcanvasNavbar-expand-xxl"
+          className="offcanvas offcanvas-end"
+          aria-labelledby="offcanvasNavbarLabel-expand-xxl"
+        >
+          <div className="offcanvas-header">
+            <div
+              className="offcanvas-title h5"
+              id="offcanvasNavbarLabel-expand-xxl"
+            >
+              <Image
+                alt="Doctor 360 Logo"
+                fetchPriority="high"
+                width={150}
+                height={46}
+                decoding="async"
+                src={''}
+                style={{ color: "transparent" }}
+              />
+            </div>
+            <button type="button" className="btn-close" aria-label="Close"></button>
+          </div>
+
+          <div className="offcanvas-body">
+            <div className="justify-content-end flex-grow-1 pe-3 navbar-nav">
+              {/* Navigation Links */}
+              <Link
+                href="/"
+                className="navbar_navItem__52jex navbar_navItemActive__lASh4 nav-link"
+              >
+                Home
+              </Link>
+              <Link
+                href="/about-us"
+                className="navbar_navItem__52jex nav-link"
+              >
+                About Us
+              </Link>
+              <Link
+                href="/contact-us"
+                className="navbar_navItem__52jex nav-link"
+              >
+                Contact Us
+              </Link>
+              <Link
+                href="https://diagnostics.doctor360.in"
+                className="navbar_navItem__52jex nav-link"
+              >
+                Book Lab Tests
+                <span className="ant-badge ant-badge-not-a-wrapper css-14wwjjs">
+                  <sup
+                    data-show="true"
+                    className="ant-scroll-number ant-badge-dot"
+                    style={{ marginTop: "-10px", right: "0px" }}
+                  ></sup>
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+      
     </div>
   );
 }
